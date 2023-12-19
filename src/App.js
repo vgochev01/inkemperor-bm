37e9b32e16af0ead8b7d2f157a43a10bf48f2403
Valeri Gochev <valeri.gochev@tryzens.com>
2023-12-19 11:04:06 +0200

    side panel on mobile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Calendar from './components/Calendar/Calendar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faArrowLeft, faXmark } from '@fortawesome/free-solid-svg-icons';
 import './App.scss';
 import Menu from './components/Menu/Menu';
 
@@ -16,13 +16,15 @@ function App() {
 
   return (
     <div className="app-container">
-      <div className={`side-panel left-panel ${isPanelOpen ? 'open' : 'closed'}`}>
+      <div className={`side-panel ${isPanelOpen ? 'open' : 'closed'}`}>
         {isPanelOpen ? (
           <FontAwesomeIcon icon={faArrowLeft} className="toggle-icon" onClick={togglePanel} />
         ) : (
           <FontAwesomeIcon icon={faBars} className="menu-icon" onClick={togglePanel} />
         )}
+        <h2 className="mobile-logo">InkEmperor</h2>
         <div className={`panel-content ${isPanelOpen ? 'show' : ''}`}>
+          <FontAwesomeIcon icon={faXmark} size="lg" className="closeIcon" onClick={togglePanel} />
           <Menu />
         </div>
       </div>
